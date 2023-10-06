2a3e16e6de5e1b1c557805d6afd6fea56731d6ee
espogee <52884804+espogee@users.noreply.github.com>
2023-10-06 12:44:48 +0000

    Initial commit

---
+++ src/test/java/com/example/restjpademo/RestjpaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restjpademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestjpaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
